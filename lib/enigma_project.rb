56390adf78024d48672ff637eca63d11f1e3fde3
JerrelMitchell <jerrelmitchell@ymail.com>
2018-02-03 11:24:13 -0700

    Create the key generator and tested that it exists and creates a unique key

--- lib/enigma_project.rb
+++ lib/enigma_project.rb
@@ -1,1 +1,10 @@
-class
+
+class KeyGenerator
+
+attr_reader :key_gen
+
+  def initialize
+    @key_gen = rand(10000..99999)
+  end
+
+end
--- test/enigma_project_test.rb
+++ test/enigma_project_test.rb
@@ -1,1 +1,21 @@
-class
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/enigma_project"
+
+class EncryptionTest < Minitest::Test
+
+  def test_it_exists
+    key = KeyGenerator.new
+
+    assert_equal KeyGenerator, key.class
+  end
+
+  def test_it_generates_a_key
+    new_key = KeyGenerator.new
+
+    assert_equal new_key, new_key.key_gen
+  end
+
+
+
+end
