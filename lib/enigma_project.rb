1021cfaeaab071ee04c87f554384dd095e9eebf7
JerrelMitchell <jerrelmitchell@ymail.com>
2018-02-03 13:32:13 -0700

    add testing and method framework for key_offset

--- lib/enigma_project.rb
+++ lib/enigma_project.rb
@@ -4,4 +4,12 @@ class KeyGenerator
   def initialize
     @key_gen = rand(10000..99999)
   end
+
+  def key_offset
+    key_offset = @key_gen.to_s.split("")
+
+    #create subarrays of strings for A,B,C,D
+
+  end
+
 end
--- test/enigma_project_test.rb
+++ test/enigma_project_test.rb
@@ -9,9 +9,23 @@ class EncryptionTest < Minitest::Test
     assert_equal KeyGenerator, key.class
   end
 
-  def test_it_generates_a_key
+  def test_it_generates_unique_keys
+    key = KeyGenerator.new
+
+    assert_equal key.key_gen, key.key_gen
+
     new_key = KeyGenerator.new
 
-    assert_equal new_key, new_key.key_gen
+    assert_equal new_key.key_gen, new_key.key_gen
   end
+
+  def test_it_creates_an_offset
+    key = KeyGenerator.new
+    key_offset = key.key_offset
+
+    # assert_equal [], key.key_offset   #this test will make the whole test pass, comment out when trying to run next part of test
+    assert_equal ["94","44","42","26"], key.key_offset
+
+  end
+
 end
