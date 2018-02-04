6cb508fc89ee4e189b699347c416c37e0ec63a76
justinetroyke <jjtroyke@gmail.com>
2018-02-04 11:16:28 -0700

    added key rotation to assign a,b,c,d and messing with test it creates key_rotation

--- lib/enigma_project.rb
+++ lib/enigma_project.rb
@@ -1,20 +1,29 @@
 require "pry"
 
 class KeyGenerator
-  attr_reader :key_gen
+  attr_reader :key_gen,
+              :key_rotation
 
   def initialize
     @key_gen = rand(10000..99999)
   end
 
 
-  def key_offset
+  def key_rotation(_integers)
     key_offset = @key_gen.to_s.split("")
-    key_rotation_a = key_offset.index[0..1].join.to_i
-    key_rotation_b = key_offset.index[1..2].join.to_i
-    key_rotation_c = key_offset.index[2..3].join.to_i
-    key_rotation_d = key_offset.index[3..4].join.to_i
+    key_rotation_a = key_offset[0..1].join.to_i
+    key_rotation_b = key_offset[1..2].join.to_i
+    key_rotation_c = key_offset[2..3].join.to_i
+    key_rotation_d = key_offset[3..4].join.to_i
+
+    @key_rotation = {
+      a: => key_rotation_a,
+      b: => key_rotation_b,
+      c: => key_rotation_c,
+      d: => key_rotation_d
+    }
   end
+
 
   # binding.pry
 end
--- test/enigma_project_test.rb
+++ test/enigma_project_test.rb
@@ -21,11 +21,10 @@ class EncryptionTest < Minitest::Test
 
   def test_it_creates_an_offset
     key = KeyGenerator.new
-    key_offset = key.key_offset
+    key.key_rotation(1,2,3,4,5)
 
     # assert_equal [], key.key_offset   #this test will make the whole test pass, comment out when trying to run next part of test
-    assert_equal ["94","44","42","26"], key.key_offset
-
+    assert_equal {a: 12, b: 23, c: 34, d: 45}, key_rotation
   end
 
 end
