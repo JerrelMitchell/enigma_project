6ad285926b467ca8b7f9bb00a47f9161ee1dd466
justinetroyke <jjtroyke@gmail.com>
2018-02-04 10:14:29 -0700

    created key_offset a,b,c,d

--- lib/enigma_project.rb
+++ lib/enigma_project.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class KeyGenerator
   attr_reader :key_gen
 
@@ -5,11 +7,14 @@ class KeyGenerator
     @key_gen = rand(10000..99999)
   end
 
+
   def key_offset
     key_offset = @key_gen.to_s.split("")
-
-    #create subarrays of strings for A,B,C,D
-
+    key_rotation_a = key_offset.index[0..1].join.to_i
+    key_rotation_b = key_offset.index[1..2].join.to_i
+    key_rotation_c = key_offset.index[2..3].join.to_i
+    key_rotation_d = key_offset.index[3..4].join.to_i
   end
 
+  # binding.pry
 end
