43f6bc5879a102b91b5752f0a284a0090dc5d0ad
JerrelMitchell <jerrelmitchell@ymail.com>
2018-02-05 08:48:47 -0700

    update key_rotation method and test to fix instance of nil in hash

--- lib/enigma_project.rb
+++ lib/enigma_project.rb
@@ -1,43 +1,39 @@
-require "pry"
 require 'date'
+require 'pry'
 
+# KeyGenerator for first part of encryption process.
 class KeyGenerator
-  attr_reader :key_gen,
-              :key_rotation,
-              :key_rotation_a,
-              :key_rotation_b,
-              :key_rotation_c,
-              :key_rotation_d
+  attr_reader :key_gen
 
   def initialize
-    @key_gen = rand(10000..99999)
-  end
-
-  def key_offset
-    key_offset = @key_gen.to_s.split("")
-    @key_rotation_a = key_offset[0..1].join.to_i
-    @key_rotation_b = key_offset[1..2].join.to_i
-    @key_rotation_c = key_offset[2..3].join.to_i
-    @key_rotation_d = key_offset[3..4].join.to_i
+    @key_gen = rand(10_000..99_999)
   end
 
   def key_rotation
+    key_offset = @key_gen.to_s.split("")
+    key_rotation_a = key_offset[0..1].join.to_i
+    key_rotation_b = key_offset[1..2].join.to_i
+    key_rotation_c = key_offset[2..3].join.to_i
+    key_rotation_d = key_offset[3..4].join.to_i
+
     @key_rotation = {
-      a: @key_rotation_a,
-      b: @key_rotation_b,
-      c: @key_rotation_c,
-      d: @key_rotation_d
+      a: key_rotation_a,
+      b: key_rotation_b,
+      c: key_rotation_c,
+      d: key_rotation_d
     }
   end
 end
 
+# DateOffset for second part of encryption process.
 class DateOffset
   attr_reader :date
 
   def initialize
-    @date = DateTime.now
+    @date = Date.now
   end
+
   def integer_date
-    integer_date = @date.strftime("%d%m%y")
+    @integer_date = @date.strftime('%d%m%y')
   end
 end
--- test/enigma_project_test.rb
+++ test/enigma_project_test.rb
@@ -1,8 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/enigma_project'
-require 'date'
 
+# :nodoc:
 class EncryptionTest < Minitest::Test
   def test_it_exists
     key = KeyGenerator.new
@@ -20,34 +20,11 @@ class EncryptionTest < Minitest::Test
     assert_equal new_key.key_gen, new_key.key_gen
   end
 
-  def test_we_get_four_assignable_rotations
-    key = KeyGenerator.new
-    key.key_offset
-
-    assert_instance_of Fixnum, key.key_offset
-
-  end
-
-  def test_it_creates_a_key_rotation
+  def test_it_creates_a_key_rotation_as_a_hash
     key = KeyGenerator.new
     key.key_rotation
 
-
     assert_kind_of Hash, key.key_rotation
+    refute_kind_of Hash, key.key_rotation
   end
-
-  def test_date_generates
-    date = DateOffset.new
-
-    assert_instance_of DateOffset, date
-  end
-
-  def test_date_exists_as_interger
-    date = DateTime.now
-    integer_date = date.strftime("%d%m%y").to_i
-
-
-    assert_kind_of Fixnum, integer_date
-  end
-
 end
