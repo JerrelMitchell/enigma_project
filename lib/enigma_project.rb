9a0abd5f3dfc1685f4c6d8150124e1ef84502430
JerrelMitchell <jerrelmitchell@ymail.com>
2018-02-05 08:59:18 -0700

    update key rotation method to create a valid hash and refactored method

--- lib/enigma_project.rb
+++ lib/enigma_project.rb
@@ -10,17 +10,12 @@ class KeyGenerator
   end
 
   def key_rotation
-    key_offset = @key_gen.to_s.split("")
-    key_rotation_a = key_offset[0..1].join.to_i
-    key_rotation_b = key_offset[1..2].join.to_i
-    key_rotation_c = key_offset[2..3].join.to_i
-    key_rotation_d = key_offset[3..4].join.to_i
-
+    key_offset = @key_gen.to_s.split('')
     @key_rotation = {
-      a: key_rotation_a,
-      b: key_rotation_b,
-      c: key_rotation_c,
-      d: key_rotation_d
+      a: key_offset[0..1].join.to_i,
+      b: key_offset[1..2].join.to_i,
+      c: key_offset[2..3].join.to_i,
+      d: key_offset[3..4].join.to_i
     }
   end
 end
