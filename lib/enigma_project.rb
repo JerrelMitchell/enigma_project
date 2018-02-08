8de95c4b5d583642853cc06ace70e1036e6c8b59
JerrelMitchell <jerrelmitchell@ymail.com>
2018-02-07 18:03:11 -0700

    refactor methods and tests to be more readable

--- lib/enigma_project.rb
+++ lib/enigma_project.rb
@@ -1,104 +1,96 @@
 require 'date'
-
-
 # Class creating encryption process.
 class Encryptor
   attr_reader :key_gen,
               :date,
               :squared_date,
-              :rotation_key,
               :shift,
               :message
 
   def initialize
-    @key_gen = rand(10_000..99_999)
-    @date = DateTime.now
+    @key_gen      = rand(10_000..99_999)
+    @date         = DateTime.now
     @rotation_key = rotation_key
     @key_offsets  = key_offsets
     @integer_date = integer_date
     @squared_date = squared_date
     @date_offsets = date_offsets
-    @shift = shift
-    @alpha_num = alpha_num
-
+    @shift        = shift
+    @alpha_num    = alpha_num
   end
 
   def key_offsets
     key_offset = key_gen.to_s.split('')
-    key_offsets = {
-      a: key_offset[0..1].join.to_i,
+    { a: key_offset[0..1].join.to_i,
       b: key_offset[1..2].join.to_i,
       c: key_offset[2..3].join.to_i,
-      d: key_offset[3..4].join.to_i
-    }
+      d: key_offset[3..4].join.to_i }
   end
 
   def integer_date
-    integer_date = date.strftime('%d%m%y').to_i
+    date.strftime('%d%m%y').to_i
   end
 
   def integer_date_squared
     integer_date = date.strftime('%d%m%y').to_i
-    squared_date = integer_date**2
+    integer_date**2
   end
 
   def date_offsets
     integer_date = date.strftime('%d%m%y').to_i
     squared_date = integer_date**2
-    date_string = squared_date.to_s
-    date_offset = date_string[date_string.length - 4, 4].split('')
-    date_offsets = {
-      a: date_offset[0].to_i,
+    date_string  = squared_date.to_s
+    date_offset  = date_string[date_string.length - 4, 4].split('')
+    { a: date_offset[0].to_i,
       b: date_offset[1].to_i,
       c: date_offset[2].to_i,
-      d: date_offset[3].to_i
-    }
+      d: date_offset[3].to_i }
   end
 
   def rotation_key
-    rotation_key = key_offsets.merge(date_offsets) do |key, oldval, newval|
+    key_offsets.merge(date_offsets) do |_key, oldval, newval|
       newval + oldval
     end
-
   end
 
   def encrypt_a(string)
-    shift = rotation_key[:a]
+    shift     = rotation_key[:a]
     encrypt_a = Hash[alpha_num.zip(alpha_num.rotate(shift))]
-    encrypted_a = string.chars.map { |charc| encrypt_a.fetch(charc, "")}
+    string.chars.map { |charc| encrypt_a.fetch(charc, '') }
   end
 
   def encrypt_b(string)
-    shift = rotation_key[:b]
+    shift     = rotation_key[:b]
     encrypt_b = Hash[alpha_num.zip(alpha_num.rotate(shift))]
-    encrypted_b = string.chars.map { |charc| encrypt_b.fetch(charc, "")}
+    string.chars.map { |charc| encrypt_b.fetch(charc, '') }
   end
 
   def encrypt_c(string)
-    shift = rotation_key[:c]
+    shift     = rotation_key[:c]
     encrypt_c = Hash[alpha_num.zip(alpha_num.rotate(shift))]
-    encrypted_c = string.chars.map { |charc| encrypt_c.fetch(charc, "")}
+    string.chars.map { |charc| encrypt_c.fetch(charc, '') }
   end
 
   def encrypt_d(string)
-    shift = rotation_key[:d]
+    shift     = rotation_key[:d]
     encrypt_d = Hash[alpha_num.zip(alpha_num.rotate(shift))]
-    encrypted_d = string.chars.map { |charc| encrypt_d.fetch(charc, "")}
+    string.chars.map { |charc| encrypt_d.fetch(charc, '') }
   end
 
   def encrypt_four(characters)
-    split_charc = characters.split(//)
-    output_a = encrypt_a(split_charc[0])
-    output_b = encrypt_b(split_charc[1])
-    output_c = encrypt_c(split_charc[2])
-    output_d = encrypt_d(split_charc[3])
+    split_charc  = characters.split(//)
+    output_a     = encrypt_a(split_charc[0])
+    output_b     = encrypt_b(split_charc[1])
+    output_c     = encrypt_c(split_charc[2])
+    output_d     = encrypt_d(split_charc[3])
     output_total = output_a + output_b + output_c + output_d
     output_total.join
+    # binding.pry
   end
 
   def alpha_num
-    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-      "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-      "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " ", ".", ","]
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+     'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+     'w', 'x', 'y', 'z']
   end
 end
--- test/enigma_project_test.rb
+++ test/enigma_project_test.rb
@@ -7,88 +7,67 @@ require 'pry'
 class EncryptionTest < Minitest::Test
   def test_it_exists
     key = Encryptor.new
-
     assert_equal Encryptor, key.class
   end
 
   def test_it_generates_unique_keys
     key = Encryptor.new
-
     assert_equal key.key_gen, key.key_gen
 
     new_key = Encryptor.new
-
     assert_equal new_key.key_gen, new_key.key_gen
+  end
+
+  def test_it_creates_empty_hash_when_initialized
+    rotation_key = Encryptor.new
+    assert Hash, rotation_key
   end
 
   def test_it_creates_key_offsets_in_a_hash
     key = Encryptor.new
-    key.key_offsets
-
     assert_kind_of Hash, key.key_offsets
   end
 
   def test_it_creates_date_offsets_in_a_hash
     date = Encryptor.new
-    date.date_offsets
-
     assert_kind_of Hash, date.date_offsets
-  end
-
-  def test_it_creates_empty_hash_when_initialized
-    rotation_key = Encryptor.new
-
-    assert Hash, rotation_key
   end
 
   def test_it_calls_rotation_as_hash
     rotation_key = Encryptor.new
-    rotation_key.rotation_key
-
     assert Hash, rotation_key
   end
 
   def test_it_can_encrypt_character_a
     encryptor = Encryptor.new
-    first_character = "j"
-
-    assert_includes ("a".."z"), encryptor.encrypt_a(first_character).join
+    assert_includes ('a'..'z'), encryptor.encrypt_a('j').join
   end
 
   def test_it_can_encrypt_character_b
     encryptor = Encryptor.new
-    second_character = "j"
-
-    assert_includes ("a".."z"), encryptor.encrypt_b(second_character).join
+    assert_includes ('a'..'z'), encryptor.encrypt_b('j').join
   end
 
   def test_it_can_encrypt_character_c
     encryptor = Encryptor.new
-    third_character = "j"
-
-    assert_includes ("a".."z"), encryptor.encrypt_c(third_character).join
+    assert_includes ('a'..'z'), encryptor.encrypt_c('j').join
   end
 
   def test_it_can_encrypt_character_d
     encryptor = Encryptor.new
-    fourth_character = "j"
-
-
-    assert_includes ("a".."z"), encryptor.encrypt_d(fourth_character).join
+    assert_includes ('a'..'z'), encryptor.encrypt_d('j').join
   end
 
   def test_it_can_encrypt_four_characters_at_once
     encryptor = Encryptor.new
-    four_characters = "jjjj"
-
+    four_characters = 'jjjj'
     assert_kind_of String, encryptor.encrypt_four(four_characters)
   end
 
   def test_it_can_encrypt_a_long_word
     encryptor = Encryptor.new
-    four_characters = "jjjj jjjjj j jjj"
-
+    four_characters = 'jjjj jjjjj j jjj'
     assert_kind_of String, encryptor.encrypt_four(four_characters)
-  end
 
+  end
 end
